474835b75dbbd4119734dcb6a0797a5575537b2c
hunt3rh3x <hunt3rh3x@gmail.com>
2022-09-10 12:49:49 -0700

    trial1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-/**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    int integerType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    // Sizeof operator is used to evaluate the size of a variable
+    printf("Size of int: %ld bytes\n",sizeof(integerType));
+    printf("Size of float: %ld bytes\n",sizeof(floatType));
+    printf("Size of double: %ld bytes\n",sizeof(doubleType));
+    printf("Size of char: %ld byte\n",sizeof(charType));
+
+    return 0;
 }
