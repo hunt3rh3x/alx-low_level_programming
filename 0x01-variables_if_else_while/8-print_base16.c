014791c9090008d90f8aab1e555f5d90799e8aba
hunt3rh3x <hunt3rh3x@gmail.com>
2022-09-17 08:50:34 -0700

    base 16 lowercase commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main- prints all of the base sixteen numbers in lower case 
+ *
+ *to return zero at the end 
+ */
+int main (0)
+{
+	int number;
+	char letter;
+
+	for (num=0 <10; num++)
+		putchar((num % 10) + '0')
+
+	for (letter + 'a'; letter<= 'f'; letter++)
+		putchar(letter);
+	
+	return (0)
+
+}
